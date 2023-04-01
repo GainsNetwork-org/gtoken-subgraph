36d7a6e3ee1f69e043d3efcf39a3fa3872009520
thebuidler <thebuidler@pm.me>
2023-04-01 14:41:32 -0700

    Added support for contract creation transactions

--- src/utils/access/transaction.ts
+++ src/utils/access/transaction.ts
@@ -1,5 +1,6 @@
 import { Bytes, ethereum } from '@graphprotocol/graph-ts';
 import { Transaction } from '../../types/schema';
+import { ZERO_ADDRESS } from '../constants';
 
 export function generateTransactionId(transactionHash: string, logIndex: string): string {
   return transactionHash + '-' + logIndex;
@@ -9,10 +10,12 @@ export function createOrLoadTransaction(event: ethereum.Event, action: string, s
   const block = event.block;
   const ethTransaction = event.transaction;
   const logIndex = event.logIndex;
+  const receipt = event.receipt;
 
   const id = generateTransactionId(ethTransaction.hash.toHexString(), logIndex.toString());
   let transaction = Transaction.load(id);
   if (transaction == null) {
+    const contractCreation = ethTransaction.to == null && receipt?.contractAddress;
     transaction = new Transaction(id);
     transaction.logIndex = logIndex;
     transaction.from = ethTransaction.from;
@@ -20,7 +23,7 @@ export function createOrLoadTransaction(event: ethereum.Event, action: string, s
     transaction.gasLimit = ethTransaction.gasLimit;
     transaction.hash = ethTransaction.hash;
     transaction.index = ethTransaction.index;
-    transaction.to = ethTransaction.to as Bytes;
+    transaction.to = (contractCreation ? receipt.contractAddress : ethTransaction.to || ZERO_ADDRESS) as Bytes;
     transaction.value = ethTransaction.value;
     transaction.timestamp = block.timestamp;
     transaction.blockGasLimit = block.gasLimit;
