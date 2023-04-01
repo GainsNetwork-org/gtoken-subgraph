bac97570e8bae03ff4718794eefcf37736e34f54
thebuidler <thebuidler@pm.me>
2023-04-01 15:20:32 -0700

    Bugfixed wasm compiler issue

--- src/utils/access/transaction.ts
+++ src/utils/access/transaction.ts
@@ -1,4 +1,4 @@
-import { Bytes, ethereum } from '@graphprotocol/graph-ts';
+import { Bytes, ethereum, Address } from '@graphprotocol/graph-ts';
 import { Transaction } from '../../types/schema';
 import { ZERO_ADDRESS } from '../constants';
 
@@ -15,7 +15,6 @@ export function createOrLoadTransaction(event: ethereum.Event, action: string, s
   const id = generateTransactionId(ethTransaction.hash.toHexString(), logIndex.toString());
   let transaction = Transaction.load(id);
   if (transaction == null) {
-    const contractCreation = ethTransaction.to == null && receipt?.contractAddress;
     transaction = new Transaction(id);
     transaction.logIndex = logIndex;
     transaction.from = ethTransaction.from;
@@ -23,12 +22,21 @@ export function createOrLoadTransaction(event: ethereum.Event, action: string, s
     transaction.gasLimit = ethTransaction.gasLimit;
     transaction.hash = ethTransaction.hash;
     transaction.index = ethTransaction.index;
-    transaction.to = (contractCreation ? receipt.contractAddress : ethTransaction.to || ZERO_ADDRESS) as Bytes;
     transaction.value = ethTransaction.value;
     transaction.timestamp = block.timestamp;
     transaction.blockGasLimit = block.gasLimit;
     transaction.blockNumber = block.number;
     transaction.event = action;
+
+    let to = ethTransaction.to;
+    if (to === null) {
+      to = Address.fromString(ZERO_ADDRESS);
+      if (receipt != null && receipt.contractAddress !== null) {
+        to = receipt.contractAddress;
+      }
+    }
+    transaction.to = to as Bytes;
+
     if (save) {
       transaction.save();
     }
